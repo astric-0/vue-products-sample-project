6ac4ef43559c45416b7aac4a94edee055c750493
astric <nittin1f4@gmail.com>
2023-09-09 13:19:07 +0530

    Changed Grid Layout values

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,11 +5,11 @@ import ProductList from '../components/ProductList.vue';
 
 <template>
     <div class="row justify-content-center">
-        <div class="col-12 col-md-7">
+        <div class="col-12 col-md-6">
             <InputProduct />
         </div>
 
-        <div class="col-12 col-md-3">
+        <div class="col-12 col-md-5">
             <ProductList />
         </div>
     </div>
